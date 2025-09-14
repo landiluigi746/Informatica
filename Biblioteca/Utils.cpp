7a4e16f3d374069af3e676f1f94f602ec21afc2c
landiluigi746 <landiluigi865@gmail.com>
2025-09-14 23:56:21 +0200

    [feat]: Scaffold menu and utility functions for console i/o

---
+++ Biblioteca/Utils.cpp
@@ -0,0 +1,26 @@
+#include "Utils.hpp"
+
+#include <cstdlib>
+#include <ios>
+#include <iostream>
+#include <limits>
+
+namespace Utils
+{
+    void ClearScreen()
+    {
+        std::cout << std::flush;
+        std::system("cls || clear");
+    }
+
+    void Pause()
+    {
+        std::cout << "Press any button to continue..." << std::flush;
+        std::system("read -n 1 > /dev/null || pause > nul");
+    }
+
+    void ClearInputBuffer()
+    {
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
--- Biblioteca/main.cpp
+++ Biblioteca/main.cpp
@@ -1,7 +1,48 @@
+#include "Utils.hpp"
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    int choice;
+
+    do {
+        Utils::ClearScreen();
+
+        std::cout << "----------------------------------------------------\n";
+        std::cout << "|                                                  |\n";
+        std::cout << "|                    Biblioteca                    |\n";
+        std::cout << "|                                                  |\n";
+        std::cout << "----------------------------------------------------\n\n";
+
+        std::cout << "Menu:\n\n";
+
+        std::cout << "1. Add a new book\n";
+        std::cout << "2. View all books\n";
+        std::cout << "0. Exit\n\n";
+
+        std::cout << "Enter a choice: ";
+        std::cin >> choice;
+
+        Utils::ClearInputBuffer();
+        Utils::ClearScreen();
+
+        switch(choice)
+        {
+            case 1:
+            case 2:
+                std::cout << "To be implemented!\n";
+                Utils::Pause();
+                break;
+            case 0:
+                std::cout << "Goodbye!\n";
+                break;
+            default:
+                std::cout << "Invalid choice!\n";
+                Utils::Pause();
+                break;
+        }
+    } while(choice != 0);
+
     return 0;
 }
